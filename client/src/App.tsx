077a8a181b4b1cfeb129ca580300e8dd2d228671
Ronaldo Suero Correa <est.ronaldoernesto@gmail.com>
2024-04-13 02:55:58 -0400

    Ya es 13, eso significa que mientras mas me la ma... nah. Project Initialized
    Added Tailwind, React Router Dom, Folders Structure, and added @ importing

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "@pages/Login/Login";
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={ <Login/>}/>
+    </Routes>
+  )
+}
+
+export default App
